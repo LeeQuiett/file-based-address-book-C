0e9e91e85e0cea19dd066d998463ef767ba14c0b
LeeQuiett <vkdnjpo1512@naver.com>
2024-08-02 18:41:11 +0900

    파일에서 데이터 읽어서 연결리스트로 메인 메모리에 올림

--- model.c
+++ model.c
@@ -13,17 +13,41 @@ Node* head = NULL;
 void add_addr(const char* filename, Addr* addr) {
 	FILE* fp = fopen(filename, "ab");
 	if (fp == NULL) {
-		perror("Fail to file open!");
+		perror("Fail to open file for add");
 		return;
 	}
 	fwrite(addr, sizeof(Addr), 1, fp);
 	fclose(fp);
 }
 
-void delete_addr_from_name(const char* filename, const char* name) {
-
-}
-
 Node* load_data_from_file(const char* filename) {
+	FILE* fp = fopen(filename, "rb");
+	if (fp == NULL) {
+		perror("Fail to open file for load");
+		return NULL;
+	}
+	Node* head = NULL;
+	Node* tail = NULL;
+	Addr addr;
+	while (fread(&addr, sizeof(Addr), 1, fp)) {
+		Node* newNode = (Node*)malloc(sizeof(Node));
+		if (!newNode) {
+			perror("Memory alloc failed");
+			fclose(fp);
+			return NULL;
+		}
+		newNode->data = addr;
+		newNode->next = NULL;
 
+		if (head == NULL) {
+			head = newNode;
+			tail = head;
+		} 
+		else {
+			tail->next = newNode;
+			tail = newNode;
+		}
+	}
+	fclose(fp);
+	return head;
 }
